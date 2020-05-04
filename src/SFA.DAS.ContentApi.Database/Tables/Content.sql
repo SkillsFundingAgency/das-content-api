bf46fe9535f444018152379833f49ae50468e37b
Paul Howes <paul.howes@cognetics.net>
2020-05-04 11:09:38 +0100

    Changes from review

--- src/SFA.DAS.ContentApi.Database/Tables/Content.sql
+++ src/SFA.DAS.ContentApi.Database/Tables/Content.sql
@@ -5,5 +5,6 @@
     [Content] TEXT NULL, 
     [StartDate] DATETIME NOT NULL, 
     [EndDate] DATETIME NOT NULL, 
+    [Active] BIT NOT NULL DEFAULT 1, 
     CONSTRAINT [FK_Content_ContentType] FOREIGN KEY ([ContentTypeId]) REFERENCES [ContentType]([Id])
 )
