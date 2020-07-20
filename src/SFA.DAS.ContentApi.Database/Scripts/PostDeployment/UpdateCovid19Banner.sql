0aee5c9902bd545c9e06f7db4e63a87f6f67168e
Corey <corey.faulconbridge@digital.education.gov.uk>
2020-07-20 10:28:54 +0100

    Missing opening div tag

--- src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/UpdateCovid19Banner.sql
+++ src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/UpdateCovid19Banner.sql
@@ -5,7 +5,7 @@ PRINT 'Update Covid 19 banner entry';
 BEGIN TRAN;
 BEGIN TRY
 	UPDATE [dbo].[Content]
-	SET [Data]='<p class="das-notification__heading govuk-!-margin-bottom-0">Coronavirus(COVID-19): to find out how we can support you, 
+	SET [Data]='<div class="das-notification"><p class="das-notification__heading govuk-!-margin-bottom-0">Coronavirus(COVID-19): to find out how we can support you, 
 	including changes we’re making to help your apprentices continue learning,
 	<a href ="https://www.gov.uk/government/publications/coronavirus-covid-19-apprenticeship-programme-response?es_c=7B651DDC6B6986102E8BBB7918A20DDF&es_cl=53BFD9FD1669BCDAEBD12D5CDF1D9AB8&es_id=9d%c2%a3o3C" 
 	target="_blank" class="govuk-link">read our updated guidance</a>.</p></div>'
