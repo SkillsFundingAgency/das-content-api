dc1984e92a903b8ff8df5daa8cb5f7a631449e3a
Paul Graham <paul.graham@coprime.co.uk>
2021-09-02 18:24:05 +0100

    updated Banner again

--- src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/UpdateCovid19BannerWithApprenticeEmail.sql
+++ src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/UpdateCovid19BannerWithApprenticeEmail.sql
@@ -5,10 +5,25 @@ PRINT 'Update Covid 19 banner entry (for Legacy) with Email notice';
 BEGIN TRAN;
 BEGIN TRY
 	UPDATE [dbo].[Content]
-	SET [Data]= '  <div class="info-summary" tabindex="-1"><span class="heading-small">Coronavirus (COVID-19): to find out how we can support you, including changes we''re making to help your apprentices continue learning, 
-	<a  href="https://www.gov.uk/government/publications/coronavirus-covid-19-apprenticeship-programme-response?es_c=7B651DDC6B6986102E8BBB7918A20DDF&es_cl=53BFD9FD1669BCDAEBD12D5CDF1D9AB8&es_id=9d%c2%a3o3C" target="_blank">read our updated guidance</a>.</span>
-	<p><span class="heading-small">From 9 September, you or the employer must add a unique email address for each apprentice you add. The apprentice will then receive an email invitation to create a ''My apprenticeship'' account and confirm their apprenticeship details.
-	</span></p></div>'
+	SET [Data]= '<p></p>
+        <div class="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
+            <div class="govuk-notification-banner__header">
+                <h2 class="govuk-notification-banner__title" id="govuk-notification-banner-title">
+                    Important
+                </h2>
+            </div>
+            <div class="govuk-notification-banner__content">
+                <p class="govuk-notification-banner__heading">
+                    Coronavirus (COVID-19): to find out how we can support you, including changes we''re making to help your apprentices continue learning, <a href="https://www.gov.uk/government/publications/coronavirus-covid-19-apprenticeship-programme-response?es_c=7B651DDC6B6986102E8BBB7918A20DDF&es_cl=53BFD9FD1669BCDAEBD12D5CDF1D9AB8&es_id=9d%c2%a3o3C" target="_blank">read our updated guidance</a>.
+                </p>
+            </div>
+
+            <div class="govuk-notification-banner__content">
+                <p class="govuk-notification-banner__heading">
+                    From 9 September, you or the training provider must add a unique email address for each apprentice you add. The apprentice will then receive an email invitation to create a ''My apprenticeship'' account.
+                </p>
+            </div>
+        </div>'
 	WHERE [Id] = 4
 			
 COMMIT TRAN;
