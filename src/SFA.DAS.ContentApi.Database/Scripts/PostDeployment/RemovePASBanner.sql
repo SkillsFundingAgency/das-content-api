c0839a5bd3171c2b482fbb016b7bb87263271979
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-01-24 13:29:24 +0000

    Remove PAS Banner content

---
+++ src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/RemovePASBanner.sql
@@ -0,0 +1,27 @@
+﻿SET NOCOUNT ON;
+
+PRINT 'Update PAS banner to blank to not display';
+
+BEGIN TRAN;
+BEGIN TRY
+	UPDATE [dbo].[Content]
+	SET [Data]= ''
+	WHERE [Id] = 3		
+	
+	UPDATE [dbo].[Content]
+	SET [Data]= ''
+	WHERE [Id] = 4	
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+	
+	PRINT Error_message();
+	PRINT 'PAS Banner not removed, see error info';
+	PRINT 'Rolling back transaction';
+	
+	ROLLBACK TRAN;
+
+	THROW;
+
+END CATCH;
