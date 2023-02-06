c3bdaae9314ad342194ae53c817b182f8ededa57
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-02-06 13:03:00 +0000

    remove provider banner as its been hardcoded into PAS

--- src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/NAW_Banner_2023.sql
+++ src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/NAW_Banner_2023.sql
@@ -5,59 +5,7 @@ PRINT 'Update NAW banner entry (for Legacy) with Email notice';
 BEGIN TRAN;
 
 
-BEGIN TRY
-	-- Insert PAS Legacy banner
-	IF NOT EXISTS(SELECT ID FROM [dbo].[Content] WHERE Id = 4)
-	BEGIN
-		SET IDENTITY_INSERT [dbo].[Content] ON
-		INSERT INTO [dbo].[Content] ([Id], [ContentTypeId], [Data])
-		SELECT 4, 1, '<p></p>
-        <div class="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
-            <div class="govuk-notification-banner__header">
-                <h2 class="govuk-notification-banner__title" id="govuk-notification-banner-title">
-                    Reminder
-                </h2>
-            </div>
-            <div class="govuk-notification-banner__content">
-                <p class="govuk-notification-banner__heading">
-                    National Apprenticeship Week: 6 to 12 February 2023
-                </p>
-                <p>Time to celebrate apprenticeships. Get involved with <a href="https://naw.appawards.co.uk/" target="_blank">National Apprenticeship Week</a>.</p>
-            </div>
-        </div>'
-		SET IDENTITY_INSERT [dbo].[Content] OFF
-		
-		SET IDENTITY_INSERT [dbo].[ApplicationContent] ON
-		INSERT INTO [dbo].[ApplicationContent] ([Id], [ApplicationId], [ContentId]) SELECT 4, 4, 4
-		SET IDENTITY_INSERT [dbo].[ApplicationContent] OFF
-
-		PRINT 'NAW legacy banner Provider Apprenticeship Service (Legacy) entry added';
-	END
-	ELSE
-	BEGIN
-    -- Update PAS Legacy banner
-	UPDATE [dbo].[Content]
-	SET [Data]= '<p></p>
-        <div class="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
-            <div class="govuk-notification-banner__header">
-                <h2 class="govuk-notification-banner__title" id="govuk-notification-banner-title">
-                    Reminder
-                </h2>
-            </div>
-            <div class="govuk-notification-banner__content">
-                <p class="govuk-notification-banner__heading">
-                    National Apprenticeship Week: 6 to 12 February 2023
-                </p>
-                <p>Time to celebrate apprenticeships. Get involved with <a href="https://naw.appawards.co.uk/" target="_blank">National Apprenticeship Week</a>.</p>
-            </div>
-        </div>', Active = 1
-	WHERE [Id] = 4
-	END
-	
-	
-	----!!! NO PAS BANNER LOCATION FOR CDN STYLE !!!----
-
-    
+BEGIN TRY  
 	
 	-- EAS - new govuk design styling which is shown on CDN views
 	IF NOT EXISTS(SELECT ID FROM [dbo].[Content] WHERE Id = 1)
