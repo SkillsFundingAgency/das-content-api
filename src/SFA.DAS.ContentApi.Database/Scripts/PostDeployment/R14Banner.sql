e2e01624706d72f8d0efd6d25c298cadbf977c77
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2023-10-09 10:48:48 +0100

    second paragraph and correct paragraph styling

--- src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/R14Banner.sql
+++ src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/R14Banner.sql
@@ -13,9 +13,12 @@ BEGIN TRY
                 </h2>
             </div>          
             <div class="govuk-notification-banner__content">
-                <p class="govuk-notification-banner__heading">
+                <p class="govuk-body">
                     Due to high volumes of ILR submissions, you may experience a <a href="https://help.apprenticeships.education.gov.uk/hc/en-gb/articles/14129491200530-Short-delay-between-submitting-ILR-and-data-locks">short delay between submitting your ILR and data locks being generated.</a> Please submit your files as early as possible.
                 </p>
+	            <p class="govuk-body">
+	                You must submit your final ILR for the 2022 to 2023 academic year (R14) by 6pm on Thursday 19th October 2023. Check that all apprentice data is correct before you submit.
+	            </p>
             </div>
         </div>'
         , Active = 1
