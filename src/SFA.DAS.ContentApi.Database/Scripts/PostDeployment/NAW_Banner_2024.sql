27ee6d492f63921bb9ea7267cb0feb9f854d2bc3
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2023-12-22 15:37:47 +0000

    update script remove old banner entry

--- src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/NAW_Banner_2024.sql
+++ src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/NAW_Banner_2024.sql
@@ -1,6 +1,6 @@
 ﻿SET NOCOUNT ON;
 
-PRINT 'Update NAW banner entry (for Legacy) with Email notice';
+PRINT 'Update NAW banner entry';
 
 BEGIN TRAN;
 
@@ -44,51 +44,13 @@ BEGIN TRY
         </div>',Active = 1, StartDate = '2024-01-05', EndDate = '2024-02-06'
 	WHERE [Id] = 1 
 	END
-
-	-- EAS - new govuk design styling which is shown on non-CDN views with local stylesheets
-	IF NOT EXISTS(SELECT ID FROM [dbo].[Content] WHERE Id = 2)
-	BEGIN
-		SET IDENTITY_INSERT [dbo].[Content] ON
-		INSERT INTO [dbo].[Content] ([Id], [ContentTypeId], [Data], [StartDate], [EndDate])
-		SELECT 2,1,
-               N'<div class="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
-                                     <div class="govuk-notification-banner__header">
-                                         <h2 class="govuk-notification-banner__title" id="govuk-notification-banner-title">Important notice</h2>
-                                     </div>
-                                     <div class="govuk-notification-banner__content">
-                                         <p class="govuk-notification-banner__heading">National Apprenticeship Week</p>
-                                         <p class="govuk-body">National Apprenticeship Week 2024 will run from Monday 5 to Sunday 11 February – find out how to get involved and download the toolkit at <a href="https://naw.appawards.co.uk/" target="_blank">National Apprenticeship Week</a>.</p>
-                                     </div>
-                                 </div>', '2024-01-05','2024-02-06'
-		SET IDENTITY_INSERT [dbo].[Content] OFF
-		
-		SET IDENTITY_INSERT [dbo].[ApplicationContent] ON
-		INSERT INTO [dbo].[ApplicationContent] ([Id], [ApplicationId], [ContentId]) SELECT 2, 2, 2
-		SET IDENTITY_INSERT [dbo].[ApplicationContent] OFF
-
-		PRINT 'NAW 2024 legacy banner entry added';
-	END
-	ELSE
-	BEGIN
-	UPDATE [dbo].[Content]
-	SET [Data]=N'<div class="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
-            <div class="govuk-notification-banner__header">
-                <h2 class="govuk-notification-banner__title" id="govuk-notification-banner-title">Important notice</h2>
-            </div>
-            <div class="govuk-notification-banner__content">
-                <p class="govuk-notification-banner__heading">National Apprenticeship Week</p>
-                <p class="govuk-body">National Apprenticeship Week 2024 will run from Monday 5 to Sunday 11 February – find out how to get involved and download the toolkit at <a href="https://naw.appawards.co.uk/" target="_blank">National Apprenticeship Week</a>.</p>
-            </div>
-        </div>',Active = 1,StartDate = '2024-01-05',EndDate = '2024-02-06'
-	WHERE [Id] = 2
-	END
 COMMIT TRAN;
 
 END TRY
 BEGIN CATCH
 	
 	PRINT Error_message();
-	PRINT 'NAW entry update with email NOT updated see above error';
+	PRINT 'NAW entry update NOT updated see above error';
 	PRINT 'Rolling back transaction';
 	
 	ROLLBACK TRAN;
