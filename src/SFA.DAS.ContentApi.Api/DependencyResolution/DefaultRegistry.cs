0234b1edc4a5dbd06aa30dac3a85fd7329c56daa
Jon Harris <jon.harris@digital.education.gov.uk>
2024-04-11 07:58:33 +0100

    Upgrading to Net8 and package bumps

--- src/SFA.DAS.ContentApi.Api/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.ContentApi.Api/DependencyResolution/DefaultRegistry.cs
@@ -1,22 +1,21 @@
 ﻿using SFA.DAS.ContentApi.Data;
 using StructureMap;
 
-namespace SFA.DAS.ContentApi.Api.DependencyResolution
+namespace SFA.DAS.ContentApi.Api.DependencyResolution;
+
+public class DefaultRegistry : Registry
 {
-    public class DefaultRegistry : Registry
+    private const string ServiceName = "SFA.DAS.ContentApi";
+
+    public DefaultRegistry()
     {
-        private const string ServiceName = "SFA.DAS.ContentApi";
+        Scan(
+            scan =>
+            {
+                scan.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith(ServiceName));
+                scan.RegisterConcreteTypesAgainstTheFirstInterface();
+            });
 
-        public DefaultRegistry()
-        {
-            Scan(
-                scan =>
-                {
-                    scan.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith(ServiceName));
-                    scan.RegisterConcreteTypesAgainstTheFirstInterface();
-                });
-
-            For<IContentApiDbContextFactory>().Use<DbContextWithNewTransactionFactory>();
-        }
+        For<IContentApiDbContextFactory>().Use<DbContextWithNewTransactionFactory>();
     }
 }
