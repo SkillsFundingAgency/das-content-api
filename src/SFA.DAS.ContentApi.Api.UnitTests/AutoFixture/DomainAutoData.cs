ff3207af98b64ca6d63324135c11c0ebe8c47715
Jon Harris <jon.harris@fuitsu.com>
2024-12-23 09:50:12 +0000

    Tidying/refactor/improvements

--- src/SFA.DAS.ContentApi.Api.UnitTests/AutoFixture/DomainAutoData.cs
+++ src/SFA.DAS.ContentApi.Api.UnitTests/AutoFixture/DomainAutoData.cs
@@ -3,9 +3,4 @@ using AutoFixture.NUnit3;
 
 namespace SFA.DAS.ContentApi.Api.UnitTests.AutoFixture;
 
-public class DomainAutoDataAttribute : AutoDataAttribute
-{
-    public DomainAutoDataAttribute() : base(() => new Fixture().Customize(new DomainCustomizations()))
-    {
-    }
-}
+public class DomainAutoDataAttribute() : AutoDataAttribute(() => new Fixture().Customize(new DomainCustomizations()));
