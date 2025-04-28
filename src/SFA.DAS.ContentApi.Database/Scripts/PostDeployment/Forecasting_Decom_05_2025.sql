a9cca1d522f83389f01f5d1ab09b311c105c1599
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-04-28 16:43:49 +0100

    forecasting decom banner

---
+++ src/SFA.DAS.ContentApi.Database/Scripts/PostDeployment/Forecasting_Decom_05_2025.sql
@@ -0,0 +1,42 @@
+SET NOCOUNT ON;
+
+PRINT 'Update Forecasting decommission banner';
+
+DECLARE @bannerContent  VARCHAR(MAX) = 
+N'<div class="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
+    <div class="govuk-notification-banner__header">
+        <h2 class="govuk-notification-banner__title" id="govuk-notification-banner-title">Important</h2>
+    </div>
+    <div class="govuk-notification-banner__content">
+        <p class="govuk-notification-banner__heading">Your Finance section is changing</p>
+        <p class="govuk-body">From 31 May 2025 you will no longer see the funding projection page and other features in the Finance section of your employer account. <a href="https://example.com/faq" target="_blank">Find out what''s changing on our FAQ page.</a></p>
+    </div>
+</div>';
+
+BEGIN TRAN;
+
+BEGIN TRY
+    IF NOT EXISTS(SELECT ID FROM [dbo].[Content] WHERE Id = 1)
+    BEGIN
+        SET IDENTITY_INSERT [dbo].[Content] ON
+        INSERT INTO [dbo].[Content] ([Id], [ContentTypeId], [Data])
+        SELECT 1, 1, @bannerContent
+        SET IDENTITY_INSERT [dbo].[Content] OFF
+        PRINT 'EAS Forecasting decommission banner entry added';
+    END
+    ELSE
+    BEGIN
+        UPDATE [dbo].[Content]
+        SET [Data]=@bannerContent, Active = 1, StartDate = NULL, EndDate = NULL
+        WHERE [Id] = 1
+        PRINT 'EAS Forecasting decommission banner entry updated';
+    END
+    COMMIT TRAN;
+END TRY
+BEGIN CATCH
+    PRINT Error_message();
+    PRINT 'EAS Forecasting decommission banner entry update NOT updated see above error';
+    PRINT 'Rolling back transaction';
+    ROLLBACK TRAN;
+    THROW;
+END CATCH; 
